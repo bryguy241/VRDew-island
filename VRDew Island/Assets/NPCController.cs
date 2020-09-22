ee5c0959ba02059e8b8d20a3cfcf6e060ce140f5
Penguino <motpenguinhi5@gmail.com>
2020-09-21 22:27:56 -0600

    Added in temporary NPC

---
+++ VRDew Island/Assets/NPCController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 2f;
+    public GameObject pointA, pointB;
+    public GameObject targetTile;
+    private bool hasPlanted;
+    private Vector3 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        hasPlanted = false;
+    }
+
+    private void OnEnable()
+    {
+        Vector3 start = pointA.transform.position;
+        start.y = gameObject.transform.position.y;
+        gameObject.transform.position = start;
+        Vector3 lookDir = pointB.transform.position - pointA.transform.position;
+        lookDir.y = 0;
+        lookDir.Normalize();
+        gameObject.transform.rotation = Quaternion.LookRotation(lookDir);
+        dir = lookDir;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(gameObject.transform.position, pointB.transform.position) > 2f)
+        {
+            rb.velocity = dir * speed;
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            if (!hasPlanted)
+            {
+                hasPlanted = true;
+                targetTile.GetComponent<TileSystem>().TillTile();
+                targetTile.GetComponent<TileSystem>().PlantCrop("Yam Seed");
+                targetTile.GetComponent<TileSystem>().WaterTile();
+            }
+        }
+    }
+}
---
+++ VRDew Island/Assets/NPCTextBox.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCTextBox : MonoBehaviour
+{
+
+    public GameObject npc, player;
+    public GameObject canvas;
+    public Text page;
+    public string text1, text2;
+    private bool gotItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        page.text = text1;
+        canvas.SetActive(false);
+        gotItems = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gotItems && Vector3.Distance(npc.transform.position, player.transform.position) <= 3f)
+        {
+            if (!canvas.activeSelf)
+            {
+                canvas.SetActive(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (page.text.Equals(text1))
+                {
+                    page.text = text2;
+                }
+                else if (page.text.Equals(text2))
+                {
+                    player.GetComponent<MainInventory>().TryAddItem("Yam Seed");
+                    canvas.SetActive(false);
+                    gotItems = true;
+                }
+            }
+        }
+        else if (canvas.activeSelf)
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
